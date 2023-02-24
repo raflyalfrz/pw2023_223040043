698884ec7df84fd049ea257782dfb9ab5ecf4ab7
muhammadraflyalfarizi <muhammadraflyalfrz24@gmail.com>
2023-02-24 20:37:05 +0700

    memperbaiki kesalahan pada tugas 2a

--- tugas/tugas2/2a.php
+++ tugas/tugas2/2a.php
@@ -5,21 +5,18 @@ $nama_belakang = "Alfarizi";
 
 
 for ($i = 1; $i <= 100; $i++) {
+    // jika nilai $i habis dibagi 3 dan 5, maka tampilkan nama lengkap
+    if ($i % 3 == 0 && $i % 5 == 0) {
+        echo $nama_depan . " " . $nama_belakang . "<br>";
+    }
     // jika nilai $i hanya habis dibagi 3, maka tampilkan nama depan
-    if ($i % 3 == 0) {
+    elseif ($i % 3 == 0) {
         echo $nama_depan . "<br>";
     }
-
     // jika nilai $i hanya habis dibagi 5, maka tampilkan nama belakang
     elseif ($i % 5 == 0) {
         echo $nama_belakang . "<br>";
     }
-
-    // jika nilai $i habis dibagi 3 dan 5, maka tampilkan nama lengkap
-    elseif ($i % 3 == 0 && $i % 5 == 0) {
-        echo $nama_depan . " " . $nama_belakang . "<br>";
-    }
-
     // jika nilai $i tidak habis dibagi 3 atau 5, maka tampilkan nilai $i itu sendiri
     else {
         echo $i . "<br>";
